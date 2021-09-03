d8577563bd5367323df0e4c62959905d940488cb
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-03 15:56:50 +0800

    style: restrict max width of styling

--- frontend/src/templates/Field/VerifiableField/components/VerificationBox.tsx
+++ frontend/src/templates/Field/VerifiableField/components/VerificationBox.tsx
@@ -28,7 +28,7 @@ const useVerificationBox = ({
 
   const onSubmitForm = handleSubmit(async (inputs) => {
     // TODO: Add API call to backend to verify OTP, then return signature.
-    return onSuccess('some signature')
+    return onSuccess(`some signature-${inputs.otp}`)
   })
 
   const onResendOtp = useCallback(() => {
@@ -86,6 +86,7 @@ export const VerificationBox = ({
             isRequired
             isReadOnly={isValid && isSubmitting}
             isInvalid={!!errors.otp}
+            maxW="18rem"
           >
             <FormLabel description={subheader}>{header}</FormLabel>
             <Flex>
